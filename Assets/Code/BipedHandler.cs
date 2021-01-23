e508024b2e2f9ff20e20d6379c4cb50ee5933c92
Aescosaurus <michaelbuslik@hotmail.com>
2021-01-23 16:12:18 -0700

    Weapon handler start.

---
+++ Assets/Code/BipedHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class BipedHandler
+	:
+	MonoBehaviour
+{
+	// 1 = right 2 = left
+	public GameObject GetHand( int side )
+	{
+		Assert.IsTrue( side == 1 || side == 2 );
+		return( transform.Find( "Body" ).Find( "Arm" + side.ToString() ).GetChild( 0 ).gameObject );
+	}
+}
---
+++ Assets/Code/WeaponHolder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHolder
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		var bh = GetComponent<BipedHandler>();
+		if( heldWeapon != null && bh != null )
+		{
+			curWeapon = Instantiate( heldWeapon,bh.GetHand( 1 ).transform );
+			curWeapon.transform.Rotate( transform.right,90.0f );
+		}
+	}
+
+	[SerializeField] GameObject heldWeapon = null;
+
+	GameObject curWeapon = null;
+}
