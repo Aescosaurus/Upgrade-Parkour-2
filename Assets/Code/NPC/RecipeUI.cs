03d2a75c0d82bb7056261da0d38ef41e0109533c
Aescosaurus <michaelbuslik@hotmail.com>
2021-02-09 18:17:20 -0700

    Recipe prefab for easier vendor design.

---
+++ Assets/Code/NPC/RecipeUI.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecipeUI
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		var ingredientArea = transform.Find( "Ingredients" );
+		for( int i = 0; i < ingredientArea.childCount; ++i )
+		{
+			ingredientSpots.Add( ingredientArea.GetChild( i ).GetComponent<InventorySlot>() );
+		}
+		resultSpot = transform.Find( "Results" ).GetChild( 0 ).GetComponent<InventorySlot>();
+
+		while( ingredients.Count < 3 ) ingredients.Add( null );
+		while( ingredientQuantities.Count < 3 ) ingredientQuantities.Add( 0 );
+
+		for( int i = 0; i < ingredientSpots.Count; ++i )
+		{
+			if( ingredients[i] != null )
+			{
+				transform.Find( "Ingredient" + ( i + 1 ).ToString() + "Text" )
+					.GetComponent<Text>().text = ingredients[i].GetInvName();
+			}
+			else
+			{
+				ingredientSpots[i].gameObject.SetActive( false );
+			}
+		}
+		if( result != null ) transform.Find( "ResultText" ).GetComponent<Text>().text = result.GetInvName();
+	}
+
+	public void TryExchange()
+	{
+		bool success = true;
+		for( int i = 0; i < ingredientSpots.Count; ++i )
+		{
+			if( !CheckSpotMatch( ingredients[i],ingredientSpots[i],ingredientQuantities[i] ) )
+			{
+				success = false;
+				break;
+			}
+		}
+
+		if( success )
+		{
+			for( int i = 0; i < ingredientSpots.Count; ++i )
+			{
+				ingredientSpots[i].RemoveItem( ingredientQuantities[i] );
+			}
+
+			resultSpot.AddItem( result,resultQuantity );
+		}
+	}
+
+	bool CheckSpotMatch( LoadableItem desired,InventorySlot slot,int quantity )
+	{
+		if( desired == null || slot == null ) return( true );
+		else return( desired.GetSrc() == slot.GetItem().GetSrc() && slot.CountItems() >= quantity );
+	}
+
+	public List<InventorySlot> GetAllItemSlots()
+	{
+		var itemSlots = new List<InventorySlot>();
+		itemSlots.AddRange( ingredientSpots );
+		itemSlots.Add( resultSpot );
+		return( itemSlots );
+	}
+
+	[SerializeField] List<LoadableItem> ingredients = new List<LoadableItem>();
+	[SerializeField] List<int> ingredientQuantities = new List<int>();
+	[SerializeField] LoadableItem result = null;
+	[SerializeField] int resultQuantity = 0;
+
+	List<InventorySlot> ingredientSpots = new List<InventorySlot>();
+	InventorySlot resultSpot;
+}
--- Assets/Code/NPC/VendorUIBase.cs
+++ Assets/Code/NPC/VendorUIBase.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class VendorUIBase
+public class VendorUIBase
 	:
 	MonoBehaviour
 {
 	public virtual void CloseUI()
 	{
 		vendor.GetComponent<NPCVendor>().CloseUI();
+
+		var recipes = GetComponentsInChildren<RecipeUI>();
+		foreach( var recipe in recipes )
+		{
+			var recipeSlots = recipe.GetComponent<RecipeUI>().GetAllItemSlots();
+			foreach( var item in recipeSlots )
+			{
+				DropItems( item );
+			}
+		}
 	}
 
 	protected void DropItems( InventorySlot slot )
