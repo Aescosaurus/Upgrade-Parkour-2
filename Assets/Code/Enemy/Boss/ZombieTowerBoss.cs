0dc0315657405340af9c07badb23f53bfdb4c8fb
Aescosaurus <michaelbuslik@hotmail.com>
2021-02-21 10:42:50 -0700

    Zombie tower boss ai start.

--- Assets/Code/Enemy/Boss/ZombieTowerBoss.cs
+++ Assets/Code/Enemy/Boss/ZombieTowerBoss.cs
@@ -6,4 +6,36 @@ public class ZombieTowerBoss
     :
     BossBase
 {
+	protected override void Update()
+	{
+		base.Update();
+
+		switch( phase )
+		{
+			case 0:
+				{
+					var diff = player.transform.position - transform.position;
+					diff.y = 0.0f;
+					Move( diff );
+					if( diff.sqrMagnitude < Mathf.Pow( fireballDist,2 ) )
+					{
+						StopMoving();
+						phase = 1;
+						
+					}
+				}
+				break;
+			case 1:
+
+				break;
+		}
+	}
+
+	int phase = 0;
+
+	[Header( "Fireball Phase" )]
+	[SerializeField] float fireballDist = 10.0f;
+	[SerializeField] float fireballRotSpeed = 30.0f;
+	[SerializeField] Timer fireballRefire = new Timer( 0.2f );
+	[SerializeField] Timer fireballDuration = new Timer( 3.0f );
 }
