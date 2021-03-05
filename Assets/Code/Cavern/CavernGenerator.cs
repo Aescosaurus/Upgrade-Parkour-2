f288116f4b6f70bd1a4fbf2a1e8c2f1108543ec6
Aescosaurus <michaelbuslik@hotmail.com>
2021-03-04 19:32:41 -0700

    Basic cavern generation.

--- Assets/Code/Cavern/CavernGenerator.cs
+++ Assets/Code/Cavern/CavernGenerator.cs
@@ -6,5 +6,73 @@ public class CavernGenerator
 	:
 	MonoBehaviour
 {
+	void Start()
+	{
+		tunnelPrefab = ResLoader.Load( "Prefabs/Cavern/CavernTunnel" );
+		roomPrefab = ResLoader.Load( "Prefabs/Cavern/Room" );
+		endPrefab = ResLoader.Load( "Prefabs/Cavern/CavernEnd" );
 
+		var doors = transform.Find( "Doors" );
+
+		var tunnels = new List<int>();
+		for( int i = 0; i < doors.childCount; ++i ) tunnels.Add( Random.Range( 0,4 ) );
+
+		for( int i = 0; i < doors.childCount; ++i )
+		{
+			if( tunnels[i] == 1 || tunnels[i] == 3 )
+			{
+				GenerateTunnel( doors.GetChild( i ).Find( "TopLevel" ),CanSpawnBranches( tunnels,i,true ) );
+			}
+
+			if( tunnels[i] == 2 || tunnels[i] == 3 )
+			{
+				GenerateTunnel( doors.GetChild( i ).Find( "BotLevel" ),CanSpawnBranches( tunnels,i,false ) );
+			}
+		}
+	}
+
+	void GenerateTunnel( Transform level,bool allowBranches )
+	{
+		var tunnel = Instantiate( tunnelPrefab,transform );
+		tunnel.transform.position = level.position;
+		tunnel.transform.rotation = level.transform.rotation;
+		tunnel.transform.Rotate( Vector3.up,180.0f );
+
+		var room = Instantiate( roomPrefab,transform );
+		room.transform.position = tunnel.transform.Find( "End" ).position;
+		room.transform.rotation = tunnel.transform.rotation;
+		var exitDoor = room.GetComponent<CavernRoom>().Generate( allowBranches );
+
+		if( exitDoor != null )
+		{
+			var t2 = Instantiate( tunnelPrefab,transform );
+			t2.transform.position = exitDoor.transform.position;
+			t2.transform.rotation = exitDoor.transform.rotation;
+			// t2.transform.Rotate( Vector3.up,180.0f );
+			var endPiece = Instantiate( endPrefab,transform );
+			endPiece.transform.position = t2.transform.Find( "End" ).position;
+			endPiece.transform.rotation = t2.transform.rotation;
+			Destroy( exitDoor.gameObject );
+		}
+
+		Destroy( level.gameObject );
+	}
+
+	bool CanSpawnBranches( List<int> tunnels,int i,bool top )
+	{
+		var l = i - 1;
+		var r = i + 1;
+		if( l < 0 ) l = tunnels.Count - 1;
+		if( r >= tunnels.Count ) r = 0;
+		
+		var lval = tunnels[l];
+		var rval = tunnels[r];
+
+		if( top ) return( ( lval == 0 || lval == 2 ) && ( rval == 0 || rval == 2 ) );
+		else return( ( lval == 0 || lval == 1 ) && ( rval == 0 || rval == 1 ) );
+	}
+
+	GameObject tunnelPrefab;
+	GameObject roomPrefab;
+	GameObject endPrefab;
 }
---
+++ Assets/Code/Cavern/CavernRoom.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CavernRoom
+	:
+	MonoBehaviour
+{
+	public Transform Generate( bool allowExit )
+	{
+		if( doorMap == null )
+		{
+			doorMap = new Dictionary<int,List<List<int>>>();
+
+			var upExits = new List<List<int>>();
+			upExits.Add( new List<int>{ 3,8 } );
+			upExits.Add( new List<int>{ 11,9,4,1 } );
+			upExits.Add( new List<int>{ 10,7,2,0 } );
+			upExits.Add( new List<int>{ 10,7,5,6,4,1 } );
+			upExits.Add( new List<int>{ 11,9,6,5,2,0 } );
+			upExits.Add( new List<int>{ 8,6,4,1 } );
+			upExits.Add( new List<int>{ 8,5,2,0 } );
+			upExits.Add( new List<int>{ 11,9,6,3 } );
+			upExits.Add( new List<int>{ 10,7,5,3 } );
+			
+			var leftExits = new List<List<int>>();
+			leftExits.Add( new List<int>{ 8,5 } );
+			leftExits.Add( new List<int>{ 10,7 } );
+			leftExits.Add( new List<int>{ 8,3,0,2 } );
+			leftExits.Add( new List<int>{ 11,9,6,5 } );
+			leftExits.Add( new List<int>{ 11,9,4,1,0,2 } );
+			leftExits.Add( new List<int>{ 11,9,4,1,3,5 } );
+
+			var rightExits = new List<List<int>>();
+			rightExits.Add( new List<int>{ 11,9 } );
+			rightExits.Add( new List<int>{ 8,6 } );
+			rightExits.Add( new List<int>{ 8,3,1 } );
+			rightExits.Add( new List<int>{ 10,7,2,0,1,4 } );
+			rightExits.Add( new List<int>{ 10,7,2,0,3,6 } );
+
+			doorMap.Add( 0,upExits );
+			doorMap.Add( 2,leftExits );
+			doorMap.Add( 3,rightExits );
+		}
+
+		var exitDir = 1;
+		while( exitDir == 1 ) exitDir = Random.Range( 0,3 + 1 );
+
+		if( doorMap.ContainsKey( exitDir ) )
+		{
+			var children = new List<Transform>();
+			var walls = transform.Find( "Walls" );
+			for( var i = 0; i < walls.childCount; ++i ) children.Add( walls.GetChild( i ) );
+
+			var exitChoice = Random.Range( 0,doorMap[exitDir].Count );
+			// print( exitDir + " " + exitChoice + " " + doorMap[exitDir].Count );
+			foreach( var wall in doorMap[exitDir][exitChoice] )
+			{
+				Destroy( children[wall].gameObject );
+			}
+		}
+		
+		var doors = transform.Find( "Doors" );
+		var exitDoor = doors.GetChild( exitDir );
+		var startDoor = doors.GetChild( 1 );
+		// if( exitDir != 1 ) Destroy( doors.GetChild( exitDir ).gameObject );
+		Destroy( startDoor.gameObject );
+
+		// multiple exit dirs
+
+		if( exitDir == 1 ) exitDoor = null;
+
+		return( exitDoor );
+	}
+
+	// walls have a chance of breaking anyway
+
+	static Dictionary<int,List<List<int>>> doorMap = null;
+}
