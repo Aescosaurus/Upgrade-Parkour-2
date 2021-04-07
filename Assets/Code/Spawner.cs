839abe12be933085a103db0f5be77b3085c0be24
Aescosaurus <michaelbuslik@hotmail.com>
2021-04-07 09:47:41 -0600

    Organization and new level start.

---
+++ Assets/Code/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner
+	:
+	MonoBehaviour
+{
+	void Update()
+	{
+		if( spawnPeriod.Update( Time.deltaTime ) )
+		{
+			var obj = Instantiate( spawnPrefab );
+			obj.transform.position = transform.position;
+		}	
+	}
+
+	[SerializeField] Timer spawnPeriod = new Timer( 5.0f );
+	[SerializeField] GameObject spawnPrefab = null;
+}
