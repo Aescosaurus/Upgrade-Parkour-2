44481b09cc1384e774d1d1c86850380d310d0675
Aescosaurus <michaelbuslik@hotmail.com>
2021-04-11 16:14:14 -0600

    Play unique music on each level.

---
+++ Assets/Code/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		// todo global volume
+		var audSrc = GetComponent<AudioSource>();
+		audSrc.clip = music;
+		audSrc.loop = true;
+		audSrc.Play();
+	}
+
+	[SerializeField] AudioClip music = null;
+}
