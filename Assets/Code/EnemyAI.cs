a6ca2c6ac2ad5c1970b3236e862ba56177892b66
Aescosaurus <michaelbuslik@hotmail.com>
2021-11-30 21:15:07 -0700

    Basic flying enemy.

---
+++ Assets/Code/EnemyAI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI
+	:
+	MonoBehaviour
+{
+	enum State
+	{
+		Idle,
+		Active
+	}
+
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag( "Player" );
+		body = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		switch( state )
+		{
+			case State.Idle:
+				break;
+			case State.Active:
+				UpdateAI();
+				break;
+		}
+
+		var diff = player.transform.position - transform.position;
+		var diffXY = new Vector3( diff.x,0.0f,diff.z );
+		if( diffXY.sqrMagnitude > activateDist * activateDist )
+		{
+			state = State.Idle;
+		}
+		if( diffXY.sqrMagnitude < activateDist * activateDist )
+		{
+			state = State.Active;
+		}
+	}
+
+	protected virtual void InitAI() { }
+
+	protected virtual void UpdateAI() { }
+
+	protected GameObject player;
+	protected Rigidbody body;
+
+	[SerializeField] float activateDist = 70.0f;
+
+	State state = State.Idle;
+}
---
+++ Assets/Code/FlyingEnemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemy
+	:
+	EnemyAI
+{
+	protected override void InitAI()
+	{
+		base.InitAI();
+
+		Retarget();
+	}
+
+	protected override void UpdateAI()
+	{
+		base.UpdateAI();
+
+		var diff = moveTarget - transform.position;
+		if( diff.sqrMagnitude < Mathf.Pow( targetResetDist,2 ) )
+		{
+			Retarget();
+		}
+
+		if( targetReset.Update( Time.deltaTime ) )
+		{
+			Retarget();
+		}
+
+		if( body.velocity.sqrMagnitude < maxSpd * maxSpd ||
+			Vector3.Dot( diff,body.velocity ) < 0.0f )
+		{
+			body.AddForce( diff * accel * Time.deltaTime );
+		}
+	}
+
+	void Retarget()
+	{
+		targetReset.Reset();
+
+		var targetOffset = new Vector3(
+			Random.Range( -1.0f,1.0f ),
+			Random.Range( -1.0f,1.0f ),
+			Random.Range( -1.0f,1.0f ) ).normalized;
+		moveTarget = player.transform.position + targetOffset * targetOffsetDist.Rand();
+	}
+
+	Vector3 moveTarget = Vector3.zero;
+	float targetResetDist = 1.0f;
+
+	[SerializeField] Timer targetReset = new Timer( 3.0f );
+	[SerializeField] RangeF targetOffsetDist = new RangeF( 0.5f,5.0f );
+	[SerializeField] float accel = 500.0f;
+	[SerializeField] float maxSpd = 10.0f;
+}
